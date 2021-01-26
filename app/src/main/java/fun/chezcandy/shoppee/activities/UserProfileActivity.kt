9ab202679af09248a31f0dcb6424488fb92629c6
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-01-26 19:00:06 +0900

    ADD editProfileScreen

---
+++ app/src/main/java/fun/chezcandy/shoppee/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package `fun`.chezcandy.shoppee.activities
+
+import `fun`.chezcandy.shoppee.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
