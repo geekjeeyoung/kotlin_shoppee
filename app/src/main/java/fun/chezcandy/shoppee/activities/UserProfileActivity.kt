49df5c972f3aab235d41fc94f7475f3f32fc7dfb
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-01-27 15:17:56 +0900

    ADD imageChooser

--- app/src/main/java/fun/chezcandy/shoppee/activities/UserProfileActivity.kt
+++ app/src/main/java/fun/chezcandy/shoppee/activities/UserProfileActivity.kt
@@ -3,14 +3,26 @@ package `fun`.chezcandy.shoppee.activities
 import `fun`.chezcandy.shoppee.R
 import `fun`.chezcandy.shoppee.models.User
 import `fun`.chezcandy.shoppee.utils.Constants
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
-class UserProfileActivity : AppCompatActivity() {
+
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -31,6 +43,9 @@ class UserProfileActivity : AppCompatActivity() {
         et_email.setText(userDetails.email)
 
 
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+
+
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -41,5 +56,77 @@ class UserProfileActivity : AppCompatActivity() {
             )
         }
 
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_user_photo -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+//                        showErrorSnackBar("You already have the storage permission.", false)
+                        Constants.showImageChooser(this)
+                    } else {
+                        /* Requests permissions to be granted to this application. These permissions must be requested in your manifest, they should not be granted to your app.
+                        *   and they should have protection level. */
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSIONS_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSIONS_CODE) {
+            // If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                showErrorSnackBar("The storage permission is granted.", false)
+                Constants.showImageChooser(this)
+            } else {
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        val selectedImageFileUri = data.data!!
+
+                        iv_user_photo.setImageURI(selectedImageFileUri)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this@UserProfileActivity,
+                            resources.getString(R.string.image_selection_failed),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
     }
 }
--- app/src/main/java/fun/chezcandy/shoppee/utils/Constants.kt
+++ app/src/main/java/fun/chezcandy/shoppee/utils/Constants.kt
@@ -1,8 +1,26 @@
 package `fun`.chezcandy.shoppee.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+
 object Constants {
     const val USERS: String = "users"
+    const val READ_STORAGE_PERMISSIONS_CODE: Int = 2
     const val MYSHOPPEE_PREFERENCES: String = "MyShoppeePrefs"
     const val LOGGED_IN_USERNAME: String = "logged_in_username"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val PICK_IMAGE_REQUEST_CODE: Int = 1
+
+    fun showImageChooser(activity: Activity) {
+        // An intent for launching the image selection of phone storage.
+
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+
+        // Launches the image selection of phone storage using the constant code.
+        activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
 }
